4ea3fa15da24b9b67c0fc92ad190b5c823af044d
DonovskyiDanil <Donovskyidanil@gmail.com>
2023-12-27 22:53:47 +0200

    Update script.js
    Воспользовался методом addEventListener в файле script.js. Кнопки "ok", "weather" и "cancel" реагируют на события.

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const okButton = document.getElementById('okButton');
+  const weatherButton = document.getElementById('weatherButton');
+  const cancelButton = document.getElementById('cancelButton');
+  const radioButtons = document.querySelectorAll('input[name="inputType"]');
+  const cityInput = document.getElementById('cityInput');
+  const idInput = document.getElementById('idInput');
+
+  okButton.addEventListener('click', getWeather);
+  weatherButton.addEventListener('click', getWeather);
+  cancelButton.addEventListener('click', function () {
+    clearInputFields();
+    console.log("Cancel button clicked");
+  });
+
+  radioButtons.forEach(button => {
+    button.addEventListener('change', function () {
+      enableInput(this.value);
+    });
+  });
+});
 
 function enableInput(inputType) {
   const cityInput = document.getElementById('cityInput');
   const idInput = document.getElementById('idInput');
-
-  if (inputType === 'city') {
-    cityInput.disabled = false;
-    idInput.disabled = true;
-  } else if (inputType === 'id') {
-    cityInput.disabled = true;
-    idInput.disabled = false;
-  }
+  cityInput.disabled = inputType !== 'city';
+  idInput.disabled = inputType !== 'id';
 }
 
-function okButton() {
-  // Add the functionality for the OK button here
-  console.log("OK button clicked");
-  // You can add specific functionality for the OK button
+function clearInputFields() {
+  document.getElementById('cityInput').value = '';
+  document.getElementById('idInput').value = '';
 }
 
 function getWeather() {
-  const apiKey = '7213d230325e2b4263368cdbda58c005'; // Replace with your OpenWeatherMap API key
+  const apiKey = '7213d230325e2b4263368cdbda58c005';
   const inputType = document.querySelector('input[name="inputType"]:checked').value;
-  let input;
-
-  if (inputType === 'city') {
-    input = document.getElementById('cityInput').value;
-  } else {
-    input = document.getElementById('idInput').value;
-  }
-
+  const input = (inputType === 'city') ? document.getElementById('cityInput').value : document.getElementById('idInput').value;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
 
   fetch(apiUrl)
@@ -40,20 +46,14 @@ function getWeather() {
       return response.json();
     })
     .then(data => {
-      document.getElementById('temperature').innerText = `${data.main.temp} °C`;
-      document.getElementById('windSpeed').innerText = `${data.wind.speed} m/s`;
-      document.getElementById('humidity').innerText = `${data.main.humidity} %`;
+      displayWeather('ContainerTemp', 'Temperature', `${data.main.temp} °C`);
+      displayWeather('ContainerSpeed', 'Wind Speed', `${data.wind.speed} m/s`);
+      displayWeather('ContainerHumi', 'Humidity', `${data.main.humidity} %`);
     })
     .catch(error => console.error('Error fetching weather data:', error));
 }
 
-function cancelButton() {
-  // Add the functionality for the Cancel button here
-  console.log("Cancel button clicked");
-  // You can add specific functionality for the Cancel button
-}
-
-function createWeatherFrame(containerId, label, value) {
+function displayWeather(containerId, label, value) {
   const weatherContainer = document.createElement('div');
   weatherContainer.classList.add('weatherContainer');
 
